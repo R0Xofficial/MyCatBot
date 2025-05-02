7f39cac0ceaa6be81e0778f3c53dc045019ec589
R0X <patryk.bieganski1003@gmail.com>
2025-05-02 10:08:59 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set your BOT token here in ""
-export TELEGRAM_BOT_TOKEN="PASTE_YOUR_REAL_BOT_TOKEN_HERE"
+export TELEGRAM_BOT_TOKEN="PASTE_HERE"
 # Set your OWNER ID here in ""
-export TELEGRAM_OWNER_ID="PASTE_YOUR_REAL_NUMERICAL_ID_HERE"
+export TELEGRAM_OWNER_ID="PASTE_HERE"
 # Set your TENOR API here so that gifs appear with the commands /kill, /punch, /bite, /hug, /slap, /attack
 # Note that for this command to be executed, delete the HASHTAG before it
-# export TENOR_API_KEY="PASTE_YOUR_TENOR_API_KEY_HERE"
+# export TENOR_API_KEY="PASTE_HERE"
 
 echo "done"
