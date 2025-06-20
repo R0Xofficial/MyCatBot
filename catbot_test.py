e4f2e1f8b4a737099d911bb90f85278859101278
R0X <patryk.bieganski1003@gmail.com>
2025-06-20 18:34:09 +0200

    Update catbot_test.py

--- catbot_test.py
+++ catbot_test.py
@@ -1,3 +1,8 @@
+# MyCatBot - Telegram bot
+# Copyright (C) 2025 R0X
+# Licensed under the GNU General Public License v3.0
+# See the LICENSE file for details.
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
